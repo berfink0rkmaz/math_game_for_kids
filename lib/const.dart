40ba0774efe801a86debf2bf924e9f4948186c58
berfinkorkmaz <030722019@std.izu.edu.tr>
2025-04-05 18:12:28 +0300

    First commit

---
+++ lib/const.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+var WhiteTextStyle = const TextStyle(
+    fontWeight: FontWeight.bold,
+    fontSize: 32,
+    color: Colors.white
+);
---
+++ lib/util/my_button.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import '../const.dart';
+
+class MyButton extends StatelessWidget {
+  final String child;
+  final VoidCallback onTap;
+  var buttunColor = Colors.lightGreen[400];
+
+  MyButton({
+    super.key,
+    required this.child,
+    required this.onTap});
+
+  @override
+  Widget build(BuildContext context) {
+    if(child == 'C'){
+      buttunColor = Colors.green;
+    }else if(child == 'Del'){
+      buttunColor = Colors.red;
+    }else if(child == '='){
+      buttunColor = Colors.deepPurple;
+    }
+
+    return Padding(
+      padding: const EdgeInsets.all(4.0),
+      child: GestureDetector(
+        onTap: onTap,
+        child: Container(
+          decoration: BoxDecoration(
+            color: buttunColor,
+            borderRadius: BorderRadius.circular(4),
+          ),
+          child: Center(
+            child: Text(
+              child,
+              style: WhiteTextStyle,
+            ),
+          ),
+
+        ),
+      ),
+    );
+  }
+}
