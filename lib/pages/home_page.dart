0e0bac25970e4b803a7240996d68817e43013472
math_game_for_kids <ervaeskii41@gmail.com>
2025-04-06 17:06:14 +0300

    seventh commit

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,241 @@
-import 'dart:math';
+/*import 'dart:math';
+import 'package:flutter/material.dart';
+import 'package:math/const.dart';
+import 'package:math/util/my_button.dart';
+import 'package:math/util/result_message.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  //number pad list
+  List<String> numberPad = [
+    '7',
+    '8',
+    '9',
+    'C',
+    '4',
+    '5',
+    '6',
+    'Del',
+    '1',
+    '2',
+    '3',
+    '=',
+    ' ',
+    '0',
+    ' ',
+  ];
+
+  //number A, number B
+  int numberA = 1;
+  int numberB = 1;
+
+  // String answer
+  String userAnswer = '';
+
+  //User tapped the button
+  void buttonTapped(String button) {
+    // print('Tıklanan buton: $button'); // Hangi buton tıklandığını görmek için
+    setState(() {
+      if (button == '=') {
+        checkResult();
+      } else if (button == 'C') {
+        userAnswer = '';
+      } else if (button == 'Del') {
+        if (userAnswer.isNotEmpty) {
+          userAnswer = userAnswer.substring(0, userAnswer.length - 1);
+        }
+      } else if (userAnswer.length <= 3) {
+        userAnswer += button;
+      }
+    });
+  }
+
+  //checking the correction
+  void checkResult() {
+    if (numberA + numberB == int.parse(userAnswer)) {
+      showDialog(
+        context: context,
+        builder: (context) {
+          return ResultMessage(
+            message: 'Correct!',
+            onTap: goToNextQuestion,
+            icon: Icons.arrow_forward,
+          );
+        },
+      );
+    } else {
+      showDialog(
+        context: context,
+        builder: (context) {
+          return ResultMessage(
+            message: 'Sorry try again!',
+            onTap: goToBackQuestion,
+            icon: Icons.rotate_left,
+          );
+        },
+      );
+    }
+  }
+
+  //create rondom numbers
+  var randomNumber = Random();
+
+  @override
+  void initState() {
+    super.initState();
+    numberA = randomNumber.nextInt(99) + 1;
+    numberB = randomNumber.nextInt(99) + 1;
+  }
+
+  void goToNextQuestion() {
+    Navigator.of(context).pop();
+
+    //reset values
+    setState(() {
+      userAnswer = '';
+    });
+    //create a new guestion
+    numberA = randomNumber.nextInt(99) + 1;
+    numberB = randomNumber.nextInt(99) + 1;
+  }
+
+  void goToBackQuestion(){
+    Navigator.of(context).pop();
+
+    setState(() {
+      userAnswer = '';
+    });
+
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.green.shade300,
+      body: Column(
+        children: [
+          //level progress
+          Container(height: 140, color: Colors.green),
+          // question part
+          Expanded(
+            child: Container(
+              child: Center(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      numberA.toString() + ' + ' + numberB.toString() + ' = ',
+                      style: WhiteTextStyle,
+                    ),
+                    //answer box
+                    Container(
+                      height: 50,
+                      width: 130,
+                      decoration: BoxDecoration(
+                        color: Colors.lightGreen[400],
+                        borderRadius: BorderRadius.circular(4),
+                      ),
+                      child: Center(
+                        child: Text(userAnswer, style: WhiteTextStyle),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+          //numbers part
+          Expanded(
+            flex: 2,
+            child: Padding(
+              padding: const EdgeInsets.all(4.0),
+              child: GridView.builder(
+                shrinkWrap: true,
+                itemCount: numberPad.length,
+                //physics: const NeverScrollableScrollPhysics(),
+                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 4,
+                ),
+
+                itemBuilder: (context, index) {
+                  return MyButton(
+                    child: numberPad[index],
+                    onTap: () => buttonTapped(numberPad[index]),
+                  );
+                },
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}*/
+import 'package:flutter/material.dart';
+import 'home_page_addition.dart';
+import 'home_page_multipication.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Hangi Oyunu Oynamak İstiyorsunuz?")),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center, // Dikeyde ortalar
+          crossAxisAlignment: CrossAxisAlignment.center, // Yatayda ortalar
+          children: [
+            // Oyunlar başlığı
+            Text(
+              'Oyunlar',
+              style: TextStyle(
+                fontSize: 24,
+                color: Colors.black,
+              ),
+              //textAlign: TextAlign.center, // Yatayda da ortalar
+            ),
+            SizedBox(height: 30), // Başlık ile butonlar arasındaki boşluk
+            // Toplama Oyunu Butonu
+            ElevatedButton(
+              onPressed: () {
+                // Toplama oyununa git
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const HomePageAdd()),
+                );
+              },
+              child: Text('Toplama Oyunu'),
+            ),
+            SizedBox(height: 20), // Butonlar arasındaki boşluk
+            // Çarpma Oyunu Butonu
+            ElevatedButton(
+              onPressed: () {
+                // Çarpma oyununa git
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const HomePageMul()),
+                );
+              },
+              child: Text('Çarpma Oyunu'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+
+
+/*import 'dart:math';
 import 'package:flutter/material.dart';
 import 'package:math_game_for_kids/util/my_button.dart';
 import 'package:math_game_for_kids/util/result_message.dart';
@@ -163,4 +400,4 @@ class _HomePageState extends State<HomePage> {
       ),
     );
   }
-}
+}*/
---
+++ lib/pages/home_page_addition.dart
@@ -0,0 +1,166 @@
+import 'dart:math';
+import 'package:flutter/material.dart';
+import 'package:math_game_for_kids/util/my_button.dart';
+import 'package:math_game_for_kids/util/result_message.dart';
+import '../const.dart';
+import '../custom_drawer.dart';
+import '../preferences_service.dart';
+
+class HomePageAdd extends StatefulWidget {
+  const HomePageAdd({super.key});
+
+  @override
+  State<HomePageAdd> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePageAdd> {
+  List<String> numberPad = [
+    '1', '2', '3', 'C',
+    '4', '5', '6', 'Del',
+    '7', '8', '9', '=',
+    ' ', '0', ' ',
+  ];
+
+  int numberA = 1;
+  int numberB = 1;
+  String userAnswer = '';
+
+  int correctCount = 0;
+  int wrongCount = 0;
+
+  final PreferencesService _preferencesService = PreferencesService();
+  var randomNumber = Random();
+
+  @override
+  void initState() {
+    super.initState();
+    _preferencesService.loadScore().then((scores) {
+      setState(() {
+        correctCount = scores['correct']!;
+        wrongCount = scores['wrong']!;
+      });
+    });
+
+    numberA = randomNumber.nextInt(99) + 1;
+    numberB = randomNumber.nextInt(99) + 1;
+  }
+
+  void buttonTapped(String button) {
+    setState(() {
+      if (button == '=') {
+        checkResult();
+      } else if (button == 'C') {
+        userAnswer = '';
+      } else if (button == 'Del') {
+        if (userAnswer.isNotEmpty) {
+          userAnswer = userAnswer.substring(0, userAnswer.length - 1);
+        }
+      } else if (userAnswer.length <= 3) {
+        userAnswer += button;
+      }
+    });
+  }
+
+  void checkResult() {
+    if (numberA + numberB == int.tryParse(userAnswer)) {
+      correctCount++;
+      _preferencesService.saveScore(correctCount, wrongCount);
+      showDialog(
+        context: context,
+        builder: (context) {
+          return ResultMessage(
+            message: 'Tebrikler!',
+            onTap: goToNextQuestion,
+            icon: Icons.check_circle_outline,
+          );
+        },
+      );
+    } else {
+      wrongCount++;
+      _preferencesService.saveScore(correctCount, wrongCount);
+      showDialog(
+        context: context,
+        builder: (context) {
+          return ResultMessage(
+            message: 'Tekrar Dene!',
+            onTap: goToBackQuestion,
+            icon: Icons.refresh,
+          );
+        },
+      );
+    }
+  }
+
+  void goToNextQuestion() {
+    Navigator.of(context).pop();
+    setState(() {
+      userAnswer = '';
+      numberA = randomNumber.nextInt(99) + 1;
+      numberB = randomNumber.nextInt(99) + 1;
+    });
+  }
+
+  void goToBackQuestion() {
+    Navigator.of(context).pop();
+    setState(() {
+      userAnswer = '';
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Doğru: $correctCount | Yanlış: $wrongCount"),
+        backgroundColor: const Color(0xFFBBDEFB), // pastel buz mavisi
+        foregroundColor: Colors.black87,
+      ),
+      drawer: const CustomDrawer(),
+      backgroundColor: const Color(0xFFFFF3E0), // pastel şeftali
+      body: Column(
+        children: [
+          Container(
+            height: 200,
+            width: double.infinity,
+            decoration: const BoxDecoration(
+              color: Color(0xFFC8E6C9), // pastel nane yeşili
+              borderRadius: BorderRadius.only(
+                bottomLeft: Radius.circular(20),
+                bottomRight: Radius.circular(20),
+              ),
+            ),
+            alignment: Alignment.center,
+            child: Text(
+              '$numberA + $numberB = $userAnswer',
+              style: const TextStyle(
+                fontSize: 36,
+                fontWeight: FontWeight.bold,
+                color: Color(0xFF2E7D32), // koyu yeşil yazı
+              ),
+            ),
+          ),
+          const SizedBox(height: 16),
+          Expanded(
+            flex: 2,
+            child: Padding(
+              padding: const EdgeInsets.all(12.0),
+              child: GridView.builder(
+                itemCount: numberPad.length,
+                physics: const NeverScrollableScrollPhysics(),
+                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 4),
+                itemBuilder: (context, index) {
+                  return MyButton(
+                    child: numberPad[index],
+                    onTap: () => buttonTapped(numberPad[index]),
+                    backgroundColor: const Color(0xFFFFECB3), // pastel vanilya sarı
+                    textColor: Colors.black87,
+                  );
+                },
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
